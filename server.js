0e1de1c47f58266f34ee0f51d1cbd75c48648adf
Terminal: Select Default Profile <Terminal: Select Default Profile>
2026-02-20 18:58:11 +0530

    last

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const app = require("./src/app.js")
+const app = require("./src/app.js");
+const mongoose = require("mongoose");
 
+function connectToDb() {
+  mongoose
+    .connect(
+      "mongodb+srv://anshul:nl7EsN59eD1ZD7UM@cluster0.fmssoqo.mongodb.net/Day-6",
+    )
+    .then(() => {
+      console.log("connected to database");
+    })
 
+}
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
-    
-})
+connectToDb();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
